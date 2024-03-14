4c531a6396ca4e80fd3c21f6377ce391e1b372e5
Rafael <154633798+Fali1980@users.noreply.github.com>
2024-03-14 12:32:45 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const cardValue = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const cardSuit = ["♦", "♥", "♠", "♣"];
+
+  let randomValue = cardValue[Math.floor(Math.random() * cardValue.length)];
+  let randomSuit = cardSuit[Math.floor(Math.random() * cardSuit.length)];
+  //Sustituimos el valor de la carta
+  document.getElementById("centralColumn").innerHTML = randomValue;
+  //Sustituimos los palos desde el id de la columna html
+  document.getElementById("firstColumn").innerHTML = randomSuit;
+  document.getElementById("thirdColumn").innerHTML = randomSuit;
+  //Añadir el color rojo a diamentes y corazones desde el id
+  if (randomSuit === "♦" || randomSuit === "♥") {
+    document.getElementById("firstColumn").style.color = "red";
+    document.getElementById("thirdColumn").style.color = "red";
+  }
 };
